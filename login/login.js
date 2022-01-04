b1d594c7c89e07615ccb881358e6bb0f563f1978
Imra <imrakocis@gmail.com>
2022-01-04 20:27:18 +0100

    rijesen login ali radi smao nakon refresha

--- login/login.js
+++ login/login.js
@@ -1,67 +1,63 @@
 import react, {useEffect, useState} from "react";
-import {View, NativeBaseProvider, Text} from "native-base"
+import {View, Text, StyleSheet, SafeAreaView} from "react-native"
 import { WebView } from 'react-native-webview';
 import { render } from "react-dom";
+import LoadingService from "./services/loadingService";
+import React from "react";
 
-const requestOptions = {
+  const requestOptions = {
     method: "GET",
   }
-const LoginView = () => {
-  const [loginFormUrl , setLoginFormUrl] = useState();
-  const [loginEndResponseOnlyUrl, setEndLoginResponseOnlyUrl] = useState('');
-  const [loginResponse, setLoginResponse] = useState([]);
-
-
-  const getLoginUrl = async() => {
-    const fetchedUrl= await fetch("https://api.worldoftanks.eu/wot/auth/login/?application_id=3b94e8ffc3a72fc5fcbc1477907b386f&display=page&nofollow=1",requestOptions)
-    const url = await fetchedUrl.json();
-    setResponseUrl(url);
-  }
-  useEffect(()=>{
-    //getLoginUrl();
-    const loginUrlPromise = new Promise((resolve)=> {
-        fetch('https://api.worldoftanks.eu/wot/auth/login/?application_id=3b94e8ffc3a72fc5fcbc1477907b386f&display=page&nofollow=1',requestOptions)
-    })
-    .then((response)=> response.json())
-    .then((data)=>{
-        resolve(data)
-    })
-
-    Promise.all(loginUrlPromise).then(
-        (values) => {
-            setLoginFormUrl(values)
-        }
-    )
-    console.log(loginFormUrl);
-  }, []);
-  
-   
-  setTimeout(function(){
-    loginFormUrl && console.log(loginFormUrl);
-  },5000)
-  
-  // responseUrl && componentDidMount(){
-  //   Linking.openURL(URL).catch((err) => console.error('An error occurred', err));
-  // }
-  handleWebViewNavigationStateChange = (newNavState) => {
-    const { url } = newNavState;
-    if (!url) return;
-
-    if (url.includes('status=ok')){
-        setEndLoginResponseOnlyUrl(url);
-      console.log('pero' + loginEndResponseOnlyUrl)
+  const styles = StyleSheet.create({
+    loading: {
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
     }
-  }
+  })
+const LoginView = () =>  {
+  
+    const [loginFormUrl , setLoginFormUrl] = useState();
+    const [loginEndResponseOnlyUrl, setEndLoginResponseOnlyUrl] = useState('');
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [loginResponse, setLoginResponse] = useState([]);
 
-  
-    return (
-        
-          
-            <WebView
-            source={{ uri: loginFormUrl.data.location }}
-            onNavigationStateChange={this.handleWebViewNavigationStateChange} />
-        
-      );
-  }
-  
-export default LoginView
+    useEffect(()=>{
+      //getLoginUrl();
+      const loginUrlPromise = new Promise((resolve)=> {
+        fetch('https://api.worldoftanks.eu/wot/auth/login/?application_id=3b94e8ffc3a72fc5fcbc1477907b386f&nofollow=1&display=page',requestOptions)
+          .then((response)=> response.json())
+          .then((data)=>resolve(data))
+      })
+      
+      loginUrlPromise.then(
+        function (value) {
+          setLoginFormUrl(value)
+          loginFormUrl && setIsLoaded(true);
+        }
+      )
+    }, []);
+      
+      
+      handleWebViewNavigationStateChange = (newNavState) => {
+        const { url } = newNavState;
+        if (!url) return;
+    
+        if (url.includes('status=ok')){
+            setEndLoginResponseOnlyUrl(url);
+            const arrLogin = loginEndResponseOnlyUrl.split('&')
+            console.log(arrLogin);
+        }
+      }
+      return(
+        <SafeAreaView style = {{flex:1}}>
+          {isLoaded ? 
+          <WebView
+            source = {{uri: loginFormUrl.data.location}}
+            onNavigationStateChange = {handleWebViewNavigationStateChange}
+          /> : <Text style={styles.loading}>Loading...</Text>}
+        </SafeAreaView>
+      )
+    }
+
+  export default LoginView
