5e576f524e3c631ce2c049f25c0fa187dc3d8505
Imra <imrakocis@gmail.com>
2022-01-22 20:07:38 +0100

    personalDataScreen done

--- personal_data_screen/PersonalDataScreen.js
+++ personal_data_screen/PersonalDataScreen.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, StatusBar, Image, Button } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Image,
+  Button,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useForceUpdate from '../komponente/forceUpdate';
 const styles = StyleSheet.create({
   sammaryView: { backgroundColor: '#2d322d', flex: 1 },
@@ -63,12 +73,61 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   clanView: {
+    flex: 0.3,
+    flexDirection: 'row',
+  },
+  clanPaper: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 10,
+    flex: 0.3,
+    elevation: 5,
+    shadowRadius: 5,
+    backgroundColor: '#3b3b3b',
+    borderRadius: 10,
+  },
+  clanEmblemView: {
     flex: 0.5,
     flexDirection: 'column',
     alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    marginLeft: 10,
+    justifyContent: 'center',
+    marginLeft: 5,
   },
+  clanTagNameView: {
+    flex: 0.8,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  clanCreatedAtView: {
+    flex: 0.5,
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+  clanContainer: { flex: 1, flexDirection: 'row' },
+  clanNameText: { color: 'white' },
+  clanMembersCountText: { color: '#5c5c5c', fontSize: 12 },
+  privateDataPaper: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 10,
+    flex: 0.3,
+    elevation: 5,
+    shadowRadius: 5,
+    backgroundColor: '#3b3b3b',
+    borderRadius: 10,
+  },
+  privateDataView: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  privateDataText: { color: 'white', fontSize: 18 },
+  privateDataGoldText: { color: '#ffd700', fontSize: 18 },
   emblemView: {
     flex: 1,
     marginTop: 20,
@@ -130,6 +189,10 @@ const requestOptions = {
 };
 const PersonalDataScreen = ({ loginDataObject }) => {
   const [personalDataObject, setPersonalDataObject] = useState(null);
+  const [personalClanDataObject, setPersonalClanDataObject] = useState(null);
+  const [wins, setWins] = useState(null);
+  const [createdAtObject, setCreatedAtObject] = useState(null);
+  const [personalDataIds, setPersonaldataIds] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const forceUpdate = useForceUpdate();
 
@@ -159,6 +222,14 @@ const PersonalDataScreen = ({ loginDataObject }) => {
     const response = await fetch(personalDataUrl, requestOptions);
     return await response.json();
   };
+  const getPersonalClanData = async (clanId) => {
+    const clanResponse = await fetch(
+      'https://api.worldoftanks.eu/wot/clans/info/?application_id=3b94e8ffc3a72fc5fcbc1477907b386f&clan_id=' +
+        clanId,
+      requestOptions
+    );
+    return await clanResponse.json();
+  };
   useEffect(() => {
     loadUrl();
   }, []);
@@ -166,13 +237,43 @@ const PersonalDataScreen = ({ loginDataObject }) => {
   const loadUrl = async () => {
     const response = await getPersonalData();
     setPersonalDataObject(response);
+    var acc_id_str = loginDataObject.account_id.toString();
+    var clan_id_str = Object.byString(
+      response,
+      'data.' + acc_id_str + '.clan_id'
+    ).toString();
+    var battles = Object.byString(
+      response,
+      'data.' + acc_id_str + '.statistics.all.battles'
+    );
+    var wins = Object.byString(
+      response,
+      'data.' + acc_id_str + '.statistics.all.wins'
+    );
+    var wins_in_percent = (wins / battles) * 100;
+    const clanResponse = await getPersonalClanData(clan_id_str);
+    var clan_created_at_timestamp = Object.byString(
+      clanResponse,
+      'data.' + clan_id_str + '.created_at'
+    );
 
+    var clan_date = new Date(clan_created_at_timestamp * 1000);
+
+    setPersonalClanDataObject(clanResponse);
+    setPersonaldataIds({ acc_id_str: acc_id_str, clan_id_str: clan_id_str });
+    setCreatedAtObject({ clan_date: clan_date });
+    setWins({ wins_in_percent: wins_in_percent });
     setIsLoaded(true);
     forceUpdate();
 
-    //console.log('isloaded: ', personalDataObject);
+    //console.log('isloaded: ', personalClanDataObject);
   };
-  var acc_id_str = loginDataObject.account_id.toString();
+
+  // var acc_id_str = loginDataObject.account_id.toString();
+  // var clan_id_str = Object.byString(
+  //   personalDataObject,
+  //   'data.' + acc_id_str + '.clan_id'
+  // ).toString();
 
   return (
     <SafeAreaProvider>
@@ -181,7 +282,9 @@ const PersonalDataScreen = ({ loginDataObject }) => {
         <View style={styles.sammaryView}>
           <View style={styles.headerView}>
             <View style={styles.nicknameView}>
-              <Text style={styles.nicknameText}>imra24</Text>
+              <Text style={styles.nicknameText}>
+                {loginDataObject.nickname}
+              </Text>
             </View>
             <View style={styles.signOutView}>
               <Button onPress={() => ''} title='SIGN OUT' color='#f95813' />
@@ -192,11 +295,14 @@ const PersonalDataScreen = ({ loginDataObject }) => {
               <View style={styles.topLeftTextView}>
                 <Text style={styles.headerText}>Battles</Text>
                 <Text style={styles.statsText}>
-                  {/* {Object.byString(
+                  {Object.byString(
                     personalDataObject,
-                    'data.' + acc_id_str + '.account_id'
-                  )} */}
-                  23,256
+                    'data.' +
+                      personalDataIds.acc_id_str +
+                      '.statistics.all.battles'
+                  ).toLocaleString(undefined, {
+                    maximumFractionDigits: 2,
+                  })}
                 </Text>
               </View>
 
@@ -206,11 +312,9 @@ const PersonalDataScreen = ({ loginDataObject }) => {
               <View style={styles.topRightTextView}>
                 <Text style={styles.headerText}>Victories</Text>
                 <Text style={styles.statsText}>
-                  {/* {Object.byString(
-                    personalDataObject,
-                    'data.' + acc_id_str + '.account_id'
-                  )} */}
-                  55,55%
+                  {wins.wins_in_percent.toLocaleString(undefined, {
+                    maximumFractionDigits: 2,
+                  }) + '%'}
                 </Text>
               </View>
             </View>
@@ -241,8 +345,8 @@ const PersonalDataScreen = ({ loginDataObject }) => {
                   {/* {Object.byString(
                     personalDataObject,
                     'data.' + acc_id_str + '.account_id'
-                  )} */}
-                  720.1
+                  ).toLocaleString(undefined)} */}
+                  720.6
                 </Text>
               </View>
             </View>
@@ -252,13 +356,96 @@ const PersonalDataScreen = ({ loginDataObject }) => {
                 <Text style={styles.headerText}>10.02.2013</Text>
               </View>
               <View style={styles.createdAt}>
-                <Text style={styles.headerText}>Account created: </Text>
+                <Text style={styles.headerText}>Account created:</Text>
                 <Text style={styles.headerText}>10.02.2013</Text>
               </View>
             </View>
           </View>
-          <View style={styles.clanView}>
-            <Text>test123</Text>
+
+          <View style={styles.clanPaper}>
+            <View style={styles.clanContainer}>
+              <View style={styles.clanEmblemView}>
+                <Image
+                  source={{
+                    uri: Object.byString(
+                      personalClanDataObject,
+                      'data.' + personalDataIds.clan_id_str + '.emblems.x64.wot'
+                    ),
+                  }}
+                  style={{ height: 64, width: 64 }}></Image>
+              </View>
+              <View style={styles.clanTagNameView}>
+                <Text
+                  style={{
+                    color: Object.byString(
+                      personalClanDataObject,
+                      'data.' + personalDataIds.clan_id_str + '.color'
+                    ),
+                    fontSize: 25,
+                  }}>
+                  {'['}
+                  {Object.byString(
+                    personalClanDataObject,
+                    'data.' + personalDataIds.clan_id_str + '.tag'
+                  )}
+                  {']'}
+                </Text>
+                <Text style={styles.clanNameText}>
+                  {Object.byString(
+                    personalClanDataObject,
+                    'data.' + personalDataIds.clan_id_str + '.name'
+                  )}
+                </Text>
+                <Text style={styles.clanMembersCountText}>
+                  {'Members ' +
+                    Object.byString(
+                      personalClanDataObject,
+                      'data.' + personalDataIds.clan_id_str + '.members_count'
+                    )}
+                </Text>
+              </View>
+              <View style={styles.clanCreatedAtView}>
+                <Text style={styles.headerText}>Clan created:</Text>
+                <Text style={styles.headerText}>
+                  {createdAtObject.clan_date.getDate() +
+                    '.' +
+                    (createdAtObject.clan_date.getMonth() + 1) +
+                    '.' +
+                    createdAtObject.clan_date.getFullYear() +
+                    '.'}
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.privateDataPaper}>
+            <View style={styles.privateDataView}>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                {/* <Image
+                  style={{ width: 90, height: 40, marginTop: 2 }}
+                  source={require('../img/gold.png')}></Image> */}
+                <Text style={styles.privateDataText}>Gold: </Text>
+                <Text style={styles.privateDataGoldText}>2514</Text>
+              </View>
+            </View>
+            <View style={styles.privateDataView}>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <Text style={styles.privateDataText}>Silvers: </Text>
+                <Text style={styles.privateDataText}>85,526,456</Text>
+              </View>
+            </View>
+            <View style={styles.privateDataView}>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <Text style={styles.privateDataText}>Free Expiriance: </Text>
+                <Text style={styles.privateDataText}>126,658</Text>
+              </View>
+            </View>
+            <View style={styles.privateDataView}>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <Text style={styles.privateDataText}>Bonds: </Text>
+                <Text style={styles.privateDataText}>21,895</Text>
+              </View>
+            </View>
           </View>
         </View>
       ) : (
